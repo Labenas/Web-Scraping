b8980fa423350596c600d0ebbea5b7734eaf0e3d
Labenas <66065598+Labenas@users.noreply.github.com>
2020-12-21 08:16:11 +0200

    Add project files.

---
+++ Web Scraping/Program.cs
@@ -0,0 +1,26 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace Web_Scraping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var html = "https://www.cvonline.lt/darbo-skelbimai/informacines-technologijos/q-.net/q-.net/vilniaus?page=1";
+
+            HtmlWeb web = new HtmlWeb();
+
+            var htmlDoc = web.Load(html);
+
+            foreach (var nNode in htmlDoc.DocumentNode.SelectNodes("//h2"))
+            {
+                if (nNode.NodeType == HtmlNodeType.Element)
+                {   
+                    
+                    Console.WriteLine(nNode.InnerText);
+                }
+            }
+        }
+    }
+}
